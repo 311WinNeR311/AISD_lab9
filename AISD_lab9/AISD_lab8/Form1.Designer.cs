b1cf8ac9ad54cb616a963d4b3b0ba60e405f7d74
311WinNeR311 <32198620+311WinNeR311@users.noreply.github.com>
2017-11-12 00:04:14 +0200

    Add files via upload

---
+++ AISD_lab9/AISD_lab8/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace AISD_lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Random = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Count of elements :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Random});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(228, 434);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Random
+            // 
+            this.Random.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Random.HeaderText = "Random Numbers:";
+            this.Random.Name = "Random";
+            this.Random.ReadOnly = true;
+            this.Random.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(376, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.richTextBox1.Location = new System.Drawing.Point(246, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(237, 434);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 500);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(514, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sorting methods";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Random;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ AISD_lab9/AISD_lab8/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AISD_lab8
+{
+    public partial class Form1 : Form
+    {
+        private int[] iArray = null;
+        private uint uiSize = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Quick sort
+        static public void Swap<T>(ref T l, ref T r)
+        {
+            T temp = l;
+            l = r;
+            r = temp;
+        }
+        void QuickSort(int[] iArray)
+        {
+            QuickSort(iArray, 0, iArray.Length - 1);
+        }
+        void QuickSort(int[] iArray, int left, int right)
+        {
+            int i = left, j = right;
+            int middle = iArray[(i + j) / 2];
+            while (i <= j)
+            {
+                while (iArray[i] < middle)
+                {
+                    i++;
+                }
+                while (iArray[j] > middle)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    Swap(ref iArray[i], ref iArray[j]);
+                    i++;
+                    j--;
+                }
+            }
+            if (left < j)
+            {
+                QuickSort(iArray, left, j);
+            }
+            if (i < right)
+            {
+                QuickSort(iArray, i, right);
+            }
+        }
+
+        // Binary search
+        private bool BinarySearch(int [] iArray, int iElement, ref int iPos, ref int iCountOfComparsions)
+        {
+            iCountOfComparsions = 0;
+            bool isFounded = false;
+            int iMin = 0;
+            int iMax = iArray.Length - 1;
+            int iPosP = iPos;
+            while (iMin < iMax)
+            {
+                iPos = (iMin + iMax) / 2;
+                ++iCountOfComparsions;
+                if (iArray[iPos] < iElement)
+                    iMin = iPos + 1;
+                else
+                {
+                    iPosP = iPos;
+                    iMax = iPos;
+                    isFounded = true;
+                }
+            }
+            return isFounded;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength > 6 || !(uint.TryParse(textBox1.Text, out uint _uiSize)))
+            {
+                MessageBox.Show("Incorrect count of elements");
+                return;
+            }
+
+            uiSize = _uiSize;
+            
+            dataGridView1.Rows.Clear();
+            richTextBox1.Clear();
+
+            iArray = new int[uiSize];
+
+            Random rnd = new Random();
+            for (uint i = 0; i < uiSize; ++i)
+            {
+                iArray[i] = rnd.Next(0, 100000);
+            }
+
+            QuickSort(iArray);
+
+            for (uint i = 0; i < uiSize; ++i)
+            {
+                dataGridView1.Rows.Add(Convert.ToString(iArray[i]));
+            }
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (uiSize == 0 || iArray == null)
+            {
+                MessageBox.Show("First of all, generate random elements");
+                return;
+            }
+
+            //Searching min element(iMin)
+            int iMin = iArray[0];
+            for (uint i = 0; i < uiSize; i++)
+            {
+                if (iMin > iArray[i])
+                {
+                    iMin = iArray[i];
+                }
+            }
+
+            //Finding in an array larger or equal element for a doubled product of minimum element(iMin)
+            int iMinDoubleProduct = 2 * iMin * iMin;
+            int iPos = 0;
+            int iCountOfComparsions = 0;
+            bool isFounded = BinarySearch(iArray, iMinDoubleProduct, ref iPos, ref iCountOfComparsions);
+
+            richTextBox1.Clear();
+
+            // Output results
+            if (isFounded)
+            {
+                richTextBox1.Text += "Position of founded element: " + (iPos + 1).ToString() + "\nCount of comparsions: " + iCountOfComparsions.ToString();
+            }
+            else
+            {
+                richTextBox1.Text += "Element hasn't been founded\nCount of comparsions: " + iCountOfComparsions.ToString();
+            }
+            button2.Enabled = false;
+        }
+    }
+}
